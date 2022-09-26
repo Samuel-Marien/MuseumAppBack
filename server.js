da7cd26ca7bba26905908cf0cc87cb7d42675ad2
Samuel <timeuss.sam@gmail.com>
2022-09-26 15:29:23 +0200

    init project

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+const app = express()
+
+import dotenv from 'dotenv'
+dotenv.config()
+import 'express-async-errors'
+
+// middlewares
+import notFoundMiddleware from './middleware/not-found.js'
+import errorHandlerMiddleware from './middleware/error-handler.js'
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Welcome!')
+})
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`Server is listening on port ${port}...`))
