cb2b0dbb201bea6ccf0d5513140bee055ea83165
Samuel <timeuss.sam@gmail.com>
2022-09-26 16:41:53 +0200

    add user routes & connect to DB

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ import 'express-async-errors'
 // db and authUser
 import connectDB from './db/connect.js'
 
+// router
+import authRouter from './routes/authRoutes.js'
+
 // middlewares
 import notFoundMiddleware from './middleware/not-found.js'
 import errorHandlerMiddleware from './middleware/error-handler.js'
@@ -18,10 +21,12 @@ app.get('/', (req, res) => {
   res.send('Welcome!')
 })
 
+app.use('/api/v1/auth', authRouter)
+
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 4000
 
 const start = async () => {
   try {
