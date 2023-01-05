d9962124dfb932fe3342ff90394bc2d374c4eea0
Samuel <timeuss.sam@gmail.com>
2023-01-05 16:21:14 +0100

    Controllers[ExhibitionArtController]: add pagination

--- controllers/ExhibitionArtController.js
+++ controllers/ExhibitionArtController.js
@@ -53,10 +53,16 @@ const getAllExhibitionUserArts = async (req, res) => {
     result = result.sort('-exibitionTitle')
   }
 
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+  result = result.skip(skip).limit(limit)
+
   const arts = await result
-  res
-    .status(StatusCodes.OK)
-    .json({ arts, totalArts: arts.length, numOfPages: 1 })
+
+  const totalArts = await ExhibitionArt.countDocuments(queryObject)
+  const numOfPages = Math.ceil(totalArts / limit)
+  res.status(StatusCodes.OK).json({ arts, totalArts, numOfPages })
 }
 
 const deleteExhibitionArt = async (req, res) => {
